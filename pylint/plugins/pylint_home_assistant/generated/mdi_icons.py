280d7759e227111eee559c55a820762cd938741e
Mauro Bartolomei <62296273+Mauro2020@users.noreply.github.com>
2026-08-16 17:49:44 +0200

    Prepare ABEDOME Core 2026.9.1.dev7

--- pylint/plugins/pylint_home_assistant/generated/mdi_icons.py
+++ pylint/plugins/pylint_home_assistant/generated/mdi_icons.py
@@ -5,7 +5,7 @@ To update, run python3 -m script.hassfest
 
 from typing import Final
 
-FRONTEND_VERSION: Final[str] = "2026.8.5.dev6"
+FRONTEND_VERSION: Final[str] = "2026.8.5.dev7"
 
 MDI_ICONS: Final[set[str]] = {
     "ab-testing",
